c74c68ee6ba2a5334ee94b0224614df2006ecd5c
2301887641 <sgz19861126>
2017-06-09 15:10:15 +0800

    generation static

---
+++ src/components/admin/site/window.vue
@@ -0,0 +1,139 @@
+<template>
+  <Modal v-model="modal2" width="460">
+    <p slot="header" style="color:dodgerblue;text-align:center;font-size: 18px;">
+      <Icon type="flag"></Icon>
+      <span>页面静态化</span>
+    </p>
+    <div style="text-align:center">
+      <Button type="info" @click="aKeyGeneration">一键生成</Button>
+      <Button type="success" @click="generatIndex">生成首页</Button>
+      <Button type="warning" @click="generatArticle">文章同步</Button>
+      <Button type="error" @click="generatMenu">更新栏目</Button>
+    </div>
+    <div slot="footer">
+    </div>
+  </Modal>
+</template>
+
+<script type="text/ecmascript-6">
+  import http from '../../../assets/js/http.js';
+  export default {
+    data() {
+      return {
+        modal2: false,
+        modal_loading: false,
+      }
+    },
+    methods: {
+      aKeyGeneration() {
+        this.$Modal.confirm({
+          title: '一键生成',
+          content: '是否一键生成?',
+          okText: '生成',
+          cancelText: '取消',
+          onOk: (index) => {
+          let id = this.genaraterId;
+        this.apiGet('site/' + id).then((res) => {
+          this.handelResponse(res, (data, msg) => {
+          }, (data, msg) => {
+            this.modal_loading = false;
+            this.$Message.error(msg, 5);
+          })
+        }, (res) => {
+          //处理错误信息
+          this.modal_loading = false;
+          this.$Message.error('网络异常，请稍后重试。');
+        })
+          },
+          onCancel: () => {
+            return false
+          }
+        })
+      },
+      generatIndex() {
+        this.$Modal.confirm({
+          title: '生成首页',
+          content: '是否生成首页?',
+          okText: '生成',
+          cancelText: '取消',
+          onOk: (index) => {
+            let id = this.genaraterId;
+            this.apiGet('site/' + id).then((res) => {
+              this.handelResponse(res, (data, msg) => {
+              }, (data, msg) => {
+                this.modal_loading = false;
+                this.$Message.error(msg, 5);
+              })
+            }, (res) => {
+              //处理错误信息
+              this.modal_loading = false;
+              this.$Message.error('网络异常，请稍后重试。');
+            })
+          },
+          onCancel: () => {
+            return false
+          }
+        })
+      },
+      generatArticle() {
+        this.$Modal.confirm({
+          title: '文章同步',
+          content: '是否同步文章?',
+          okText: '同步',
+          cancelText: '取消',
+          onOk: (index) => {
+            let id = this.genaraterId;
+            this.apiGet('site/' + id).then((res) => {
+              this.handelResponse(res, (data, msg) => {
+              }, (data, msg) => {
+                this.modal_loading = false;
+                this.$Message.error(msg, 5);
+              })
+            }, (res) => {
+              //处理错误信息
+              this.modal_loading = false;
+              this.$Message.error('网络异常，请稍后重试。');
+            })
+          },
+          onCancel: () => {
+            return false
+          }
+        })
+      },
+      generatMenu() {
+        this.$Modal.confirm({
+          title: '更新栏目',
+          content: '是否更新文章?',
+          okText: '更新',
+          cancelText: '取消',
+          onOk: (index) => {
+            let id = this.genaraterId;
+            this.apiGet('site/' + id).then((res) => {
+              this.handelResponse(res, (data, msg) => {
+              }, (data, msg) => {
+                this.modal_loading = false;
+                this.$Message.error(msg, 5);
+              })
+            }, (res) => {
+              //处理错误信息
+              this.modal_loading = false;
+              this.$Message.error('网络异常，请稍后重试。');
+            })
+          },
+          onCancel: () => {
+            return false
+          }
+        })
+      }
+    },
+    props: {
+      genaraterId: Number
+    },
+    mixins: [http]
+  }
+
+</script>
+<style>
+
+
+</style>
