e4317c4a9e50e367255f0af5610c900666ea9841
yangjingzheng <1318911846@qq.com>
2017-06-12 10:26:48 +0800

    site clear

--- src/components/admin/site/site.vue
+++ src/components/admin/site/site.vue
@@ -124,6 +124,7 @@
         })
       },
       removeCache(index){
+            alert(111)
         let linkid = this.datas[index].id
         this.apiGet('Site/clearCache/'+linkid).then((res) => {
           this.handelResponse(res, (data, msg) => {
