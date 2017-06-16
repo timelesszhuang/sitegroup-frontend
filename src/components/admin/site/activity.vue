1db4dcdd210399effe906b4dfb24fc880c379bf9
2301887641 <sgz19861126>
2017-06-16 17:16:26 +0800

    local

--- src/components/admin/site/activity.vue
+++ src/components/admin/site/activity.vue
@@ -5,7 +5,54 @@
       <span>活动主题列表</span>
     </p>
     <div style="text-align:center">
-
+      <div class="ivu-table ivu-table-border"><!---->
+        <div class="ivu-table-header">
+          <table cellspacing="0" cellpadding="0" border="0" style="width:100%;">
+            <thead>
+            <tr>
+              <th>
+                <div class="ivu-table-cell"><span>id</span></div>
+              </th>
+              <th>
+                <div class="ivu-table-cell"><span>模板姓名</span></div>
+              </th>
+              <th>
+                <div class="ivu-table-cell"><span>是否同步</span></div>
+              </th>
+              <th>
+                <div class="ivu-table-cell"><span>操作</span></div>
+              </th>
+            </tr>
+            </thead>
+          </table>
+        </div>
+          <table cellspacing="0" cellpadding="0" border="0" style="width:100%;">
+            <tbody class="ivu-table-tbody">
+            <tr class="ivu-table-row" v-for="item in datas">
+              <td>
+                <div class="ivu-table-cell">
+                  <div><strong>{{item["id"]}}</strong></div>
+                </div>
+              </td>
+              <td>
+                <div class="ivu-table-cell"> <span>{{item["name"]}}</span></div>
+              </td>
+              <td class="">
+                <div class="ivu-table-cell"><span>{{item["issync"]}}</span></div>
+              </td>
+              <td class="ivu-table-column-center">
+                <div class="ivu-table-cell">
+                  <div>
+                    <button @click="sendTemp(item['id'])"  type="button" class="ivu-btn ivu-btn-primary ivu-btn-small" style="margin-right: 5px;">
+                     <span>{{item["sync"]}}</span></button>
+                    </button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
     </div>
     <div slot="footer">
     </div>
@@ -15,15 +62,28 @@
 <script type="text/ecmascript-6">
   import http from '../../../assets/js/http.js';
   export default{
-      data() {
-          return {
-            modal2: false,
-            modal_loading: false,
-          }
-      },
+    data() {
+      return {
+        modal2: false,
+        modal_loading: false,
+      }
+    },
     methods: {
-      sendTemp(index){
-        alert(index)
+      sendTemp(id){
+        this.apiGet('Site/ignoreFrontend/' + id + '/'+this.sid+"/activity").then((res) => {
+          this.handelResponse(res, (data, msg) => {
+            this.modal = false;
+            this.modal_loading = false;
+            this.$Message.success(msg);
+          }, (data, msg) => {
+            this.modal_loading = false;
+            this.$Message.error(msg, 5);
+          })
+        }, (res) => {
+          //处理错误信息
+          this.modal_loading = false;
+          this.$Message.error('网络异常，请稍后重试。');
+        })
       }
     },
     props: {
