e6868189636df868bae4970038f730f99727f29d
2301887641 <2301887641@qq.com>
2017-08-03 11:53:04 +0800

    delete 测试内容

--- src/components/sysadmin/wechatarticle/index.vue
+++ src/components/sysadmin/wechatarticle/index.vue
@@ -56,35 +56,7 @@
         editinfo: {},
         keyword_type:"",
         keywordtype:[],
-        typeNameArr:[],
-        cityList1: [
-          {
-            value: 'beijing',
-            label: '北京市'
-          },
-          {
-            value: 'shanghai',
-            label: '上海市'
-          },
-          {
-            value: 'shenzhen',
-            label: '深圳市'
-          }
-        ],
-        cityList2: [
-          {
-            value: 'hangzhou',
-            label: '杭州市'
-          },
-          {
-            value: 'nanjing',
-            label: '南京市'
-          },
-          {
-            value: 'chongqing',
-            label: '重庆市'
-          }
-        ],
+        typeNameArr:[]
       }
     },
     components: {wechatarticlesave},
