7e5fb6a5990bb9769d142169915d1ffbf8283ccf
2301887641 <2301887641@qq.com>
2017-08-04 09:50:16 +0800

    解决误删除问题

--- src/components/admin/keyword/keyword.vue
+++ src/components/admin/keyword/keyword.vue
@@ -1,25 +1,31 @@
 <!--该组件使用 饿了么 tree 实现-->
 <template>
   <div>
-    <div class="top">
-      关键词查询:
-      <Select v-model="keyword_type" style="width:200px" @on-change="changeKeyword">
-        <Option v-for="items in keywordtype" :value="items.id" :key="items.text">{{ items.text }}</Option>
-      </Select>
-      <Button type="primary" @click="queryData">查询</Button>
-    </div>
-    <div class="content" style="margin-top:10px;">
-      <Table :context="self" :border="border" :stripe="stripe" :show-header="showheader"
-             :size="size" :data="datas" :columns="tableColumns" style="width: 100%">
-      </Table>
-      <div style="margin: 10px;overflow: hidden">
-        <div style="float: right;">
-          <Page :total="total" :current="current" @on-change="changePage" @on-page-size-change="changePageSize"
-                show-total
-                show-ele vator show-sizer></Page>
-        </div>
-      </div>
-    </div>
+    <Row>
+      <Col span="24" style="padding: 10px;">
+      <Button type="primary" shape="circle" icon="android-add" @click="addkeyword">批量添加关键词</Button>
+      <Button type="success" shape="circle" icon="android-add" @click="addAkeyword">添加A类关键词</Button>
+      <Button type="error" shape="circle" icon="android-add" @click="removeKeyword">删除关键词</Button>
+      </Col>
+    </Row>
+    <el-input
+      placeholder="输入关键字进行过滤"
+      v-model="filterText"
+      style="margin-bottom: 10px;">
+    </el-input>
+    <el-tree :data="data"
+             :props="props"
+             :load="loadNode"
+             class="filter-tree"
+             lazy="" show-checkbox
+             check-strictly
+             ref="tree"
+             :filter-node-method="filterNode">
+    </el-tree>
+    <!--操作相关子组件-->
+    <Alert style="margin-top:20px;font-size:15px;font-weight: bold" type="warning">必须首先添加A类关键词,&nbsp;&nbsp;&nbsp;在A类关键词下批量添加B类关键词,&nbsp;&nbsp;&nbsp;同理,&nbsp;&nbsp;&nbsp;在B类关键词下批量添加C类关键词</Alert>
+    <keywordUpload ref="upload" :id="checkedNodeId"></keywordUpload>
+    <AkeywordAdd ref="akeywordadd"></AkeywordAdd>
   </div>
 </template>
 
@@ -30,117 +36,118 @@
   import AkeywordAdd from './Akeywordadd.vue';
 
   export default {
-//    components: {keywordUpload, AkeywordAdd},
-    data() {
-      return {
-        total: 0,
-        self: this,
-        border: true,
-        stripe: true,
-        showheader: true,
-        showIndex: true,
-        size: 'small',
-        page: 1,
-        rows: 10,
-        pageSize: 10,
-        name:'',
-        datas: [],
-        current: 1,
-        currentKeyId:0,
-        keywordtype:{},
-        keyword_type:'',
-      };
-    },
-    created() {
-      this.getData()
-      this.mainKeywordList()
-    },
-    methods: {
-      changeKeyword(key) {
-        this.currentKeyId=key
-      },
-      mainKeywordList() {
-        this.apiGet('admin/mainkeyword').then((data) => {
-          this.handelResponse(data, (data, msg) => {
-            this.keywordtype=data
-          }, (data, msg) => {
-            this.$Message.error(msg);
-          })
-        }, (data) => {
-          this.$Message.error('网络异常，请稍后重试');
-        })
-      },
-      changePage(page){
-        this.page = page;
-        this.getData();
-      },
-      changePageSize(pagesize){
-        this.rows = pagesize;
-        this.getData();
-      },
-      queryData() {
-        this.getData();
-      },
-      getData() {
-        let data = {
-          params: {
-            page: this.page,
-            rows: this.rows,
-            mainkeyword_id:this.currentKeyId,
-          }
-        }
-        this.apiGet('admin/mainkeywords', data).then((data) => {
-          this.handelResponse(data, (data, msg) => {
-            this.datas = data.rows
-            this.total = data.total;
-            this.pageSize = 10
-          }, (data, msg) => {
-            this.$Message.error(msg);
-          })
-        }, (data) => {
-          this.$Message.error('网络异常，请稍后重试');
-        })
+    watch: {
+      filterText(val) {
+        this.$refs.tree.filter(val);
       }
     },
-    computed: {
-      tableColumns()
-      {
-        let columns = [];
-        if (this.showCheckbox) {
-          columns.push({
-            type: 'selection',
-            width: 60,
-            align: 'center'
-          })
+    components: {keywordUpload, AkeywordAdd},
+    data() {
+      return {
+        checkedNodeId: 0,
+        data: [],
+        props: {
+          label: 'label',
+          children: 'children'
+        },
+        filterText: '',
+      };
+    },
+    methods: {
+      addkeyword(){
+        let node = this.$refs.tree.getCheckedNodes();
+        if (node.length == 1) {
+          if (node[0].tag == 'C') {
+            this.$Message.info('不能选择C类关键词作为上级节点');
+            return;
+          }
+          this.checkedNodeId = node[0].id;
+          this.$refs.upload.modal = true;
+        } else if (node.length == 0) {
+          this.$Message.info('请至少选择一个父亲关键词');
+        } else {
+          this.$Message.info('只能选择一个节点作为上级关键词');
         }
-        if (this.showIndex) {
-          columns.push({
-            type: 'index',
-            width: 60,
-            align: 'center'
+      },
+      removeKeyword(){
+        let node = this.$refs.tree.getCheckedNodes();
+        if (node.length == 1) {
+          let _this = this
+          this.$Modal.confirm({
+            title: '确认删除',
+            content: '您确定删除该节点?',
+            okText: '删除',
+            cancelText: '取消',
+            onOk: () => {
+              _this.apiDelete('keyword/' , node[0].id).then((res) => {
+                _this.handelResponse(res, (data, msg) => {
+                  setTimeout(function () {
+                    location.reload();
+                  }, 1000);
+                  // 删除数据库中信息
+                  _this.$Message.success(msg);
+                }, (data, msg) => {
+                  _this.$Message.error(msg);
+                })
+              }, (res) => {
+                //处理错误信息
+                _this.$Message.error('网络异常，请稍后重试');
+              })
+            },
+            onCancel: () => {
+              return false
+            }
           })
+        } else if (node.length == 0) {
+          this.$Message.info('请至少选择一个父亲关键词');
+        } else {
+          this.$Message.info('只能选择一个节点作为上级关键词');
         }
-        columns.push({
-          title: '关键词',
-          key: 'keyword',
-          sortable: true
-        });
-        columns.push({
-          title: 'A类关键词',
-          key: 'mainkeyword_name',
-          sortable: true
-        });
-        columns.push({
-          title: '出现次数',
-          key: 'count',
-          sortable: true
-        });
-        columns.push({
-          title: '时间',
-          key: 'create_time',
-          sortable: true
-        });
-        return columns;
+      },
+      //添加A 类关键词
+      addAkeyword(){
+        //添加A 类 关键词
+        this.$refs.akeywordadd.modal = true
+      },
+      //过滤数据
+      filterNode(value, data) {
+        if (!value) return true;
+        return data.label.indexOf(value) !== -1;
+      },
+      //获取数据
+      getData(id, func){
+        let data = {}
+        if (id != 0) {
+          data = {
+            params: {
+              id: id,
+            }
+          }
+        }
+        this.apiGet('keyword', data).then((res) => {
+          this.handelResponse(res, (childdata, msg) => {
+            func(childdata)
+          }, (data, msg) => {
+            this.$Message.error(msg);
+          })
+        }, (res) => {
+          //处理错误信息
+          this.$Message.error('网络异常，请稍后重试。');
+        })
+      },
+      loadNode(node, resolve) {
+        if (node.level === 0) {
+          //第一次请求资源的时候
+          this.getData(0, (data) => {
+            return resolve(data);
+          });
+        } else if (node.level >= 3) {
+          return resolve([]);
+        } else {
+          this.getData(node.data.id, (data) => {
+            resolve(data)
+          });
+        }
       }
     },
     mixins: [http]
