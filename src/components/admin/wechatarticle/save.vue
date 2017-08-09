18128147750b4907062163947e7e36252717841c
yangjingzheng <1318911846@qq.com>
2017-08-09 18:16:40 +0800

    editor

--- src/components/admin/wechatarticle/save.vue
+++ src/components/admin/wechatarticle/save.vue
@@ -24,17 +24,11 @@
               </Option>
             </Select>
             &nbsp;
-            &nbsp;
-            &nbsp;
-            &nbsp;
-            &nbsp;
-           关键词： <span style="font-size: 15px">{{form.keyword}}</span>
+            关键词： <span style="font-size: 15px">{{form.keyword}}</span>
           </Form-item>
           <Form-item label="内容" prop="content">
-            <quill-editor ref="myTextEditoredit"
-                          v-model="form.content"
-                          :config="editorOption">
-            </quill-editor>
+            <editor @change="updateData" :z-index="1":content="content"  :height="500"></editor>
+
           </Form-item>
         </Form>
       </div>
@@ -51,9 +45,9 @@
   export default {
     data() {
       return {
-        editorOption: {},
         modal: false,
         modal_loading: false,
+        contents:'',
         AddRule: {
           title: [
             {required: true, message: '请填写文章标题', trigger: 'blur'},
@@ -64,13 +58,14 @@
     computed: {
       url: function () {
         return this.form.url;
+      },
+      content:function () {
+        return this.form.content;
       }
     },
     methods: {
-      computed: {
-        editor() {
-          return this.$refs.myTextEditoredit.quillEditor
-        },
+      updateData(data) {
+       this.contents = data
       },
       changeArticletype(value) {
         this.form.articletype_name = value.label
@@ -80,13 +75,14 @@
         this.$refs.save.validate((valid) => {
           if (valid) {
             this.modal_loading = true;
+//            console.log(this.contents)
             let data = {
               articletype_id: this.form.articletype_id,
               articletype_name: this.form.articletype_name,
               auther: this.form.auther,
               summary: this.form.summary,
               title: this.form.title,
-              content: this.form.content,
+              content: this.contents,
               come_from: this.form.source,
               posttime: this.form.scrapytime
             }
