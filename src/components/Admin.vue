76f255953a5871e492d1e9cb5cc55d6783ec121b
yangjingzheng <1318911846@qq.com>
2017-08-14 10:45:54 +0800

    vue 编辑器

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -52,13 +52,16 @@
     padding-top: 10px;
     margin: 0px auto;
   }
+  .layout-hide-text .layout-text {
+    display: none;
+  }
 
 </style>
 
 <template>
-  <div class="layout" @click="menuClick" ref="menuClickEle">
+  <div class="layout":class="{'layout-hide-text': spanLeft < 4}" @click="menuClick" ref="menuClickEle">
     <Row type="flex">
-      <i-col span="4" class="layout-menu-left" style="min-width:170px">
+      <i-col :span="spanLeft" class="layout-menu-left" >
         <Menu active-name="activename" theme="dark" width="auto" :open-names="opennames" accordion>
           <div class="layout-logo-left" @click="routerChange('/admin/index')"
                style="width: 150px;height: 58px;margin: 0 auto;">
@@ -70,159 +73,159 @@
               关键词管理
             </template>
             <Menu-item name="站点关键词">
-              <Icon type="key"></Icon>
+              <Icon type="key" ></Icon>
               <span class="layout-text" @click="routerChange('/admin/keyword','站点关键词')">站点关键词</span>
             </Menu-item>
             <Menu-item name="推荐关键词库">
-              <Icon type="ios-list-outline"></Icon>
+              <Icon type="ios-list-outline" ></Icon>
               <span class="layout-text" @click="routerChange('/admin/recommendationkeyword','推荐关键词库')">推荐关键词库</span>
             </Menu-item>
             <Menu-item name="主关键词排名">
-              <Icon type="android-archive"></Icon>
+              <Icon type="android-archive" ></Icon>
               <span class="layout-text" @click="routerChange('/admin/mainkeyword','主关键词排名')">主关键词排名</span>
             </Menu-item>
           </Submenu>
           <Submenu name="8">
             <template slot="title">
-              <Icon type="ios-bookmarks-outline"></Icon>
+              <Icon type="ios-bookmarks-outline" ></Icon>
               推荐文章库
             </template>
             <Menu-item name="微信公众号关键词">
-              <Icon type="key"></Icon>
+              <Icon type="key" ></Icon>
               <span class="layout-text" @click="routerChange('/admin/wechatkeyword','微信公众号关键词')">微信公众号关键词</span>
             </Menu-item>
             <Menu-item name="微信公众号文章">
-              <Icon type="ios-bookmarks"></Icon>
+              <Icon type="ios-bookmarks" ></Icon>
               <span class="layout-text" @click="routerChange('/admin/wechatarticle','微信企业号文章')">微信公众号文章</span>
             </Menu-item>
             <Menu-item name="新闻类文章管理">
-              <Icon type="ios-copy"></Icon>
+              <Icon type="ios-copy"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/hotnews','新闻类文章管理')">新闻管理</span>
             </Menu-item>
 
           </Submenu>
           <Submenu name="2">
             <template slot="title">
-              <Icon type="ios-keypad"></Icon>
+              <Icon type="ios-keypad"  ></Icon>
               文章库
             </template>
             <Menu-item name="文章分类">
-              <Icon type="settings"></Icon>
+              <Icon type="settings"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/articletype','文章分类')">文章分类</span>
             </Menu-item>
             <Menu-item name="文章列表">
-              <Icon type="document"></Icon>
+              <Icon type="document"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/article','文章列表')">文章列表</span>
             </Menu-item>
           </Submenu>
           <Submenu name="3">
             <template slot="title">
-              <Icon type="scissors"></Icon>
+              <Icon type="scissors"  ></Icon>
               文章段落库
             </template>
             <Menu-item name="文章段落分类">
-              <Icon type="social-dropbox-outline"></Icon>
+              <Icon type="social-dropbox-outline"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/scatteredtype','文章段落分类')">文章段落分类</span>
             </Menu-item>
             <Menu-item name="文章段落">
-              <Icon type="social-dropbox-outline"></Icon>
+              <Icon type="social-dropbox-outline"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/scatteredarticle','文章段落')">文章段落</span>
             </Menu-item>
             <Menu-item name="文章段落标题">
-              <Icon type="flag"></Icon>
+              <Icon type="flag"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/title','文章段落标题')">段落标题</span>
             </Menu-item>
           </Submenu>
           <Submenu name="4">
             <template slot="title">
-              <Icon type="chatbox-working"></Icon>
+              <Icon type="chatbox-working"  ></Icon>
               问答库
             </template>
             <Menu-item name="问答分类">
-              <Icon type="settings"></Icon>
+              <Icon type="settings"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/questiontype','问答分类')">问答分类</span>
             </Menu-item>
             <Menu-item name="问答">
-              <Icon type="help"></Icon>
+              <Icon type="help"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/question','问答')">问答</span>
             </Menu-item>
           </Submenu>
           <Submenu name="5">
             <template slot="title">
-              <Icon type="briefcase"></Icon>
+              <Icon type="briefcase"  ></Icon>
               站点元素设置
             </template>
             <Menu-item name="">
-              <Icon type="drag"></Icon>
+              <Icon type="drag"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/navigation','站点栏目')">站点栏目管理</span>
             </Menu-item>
             <Menu-item name="友情链接">
-              <Icon type="drag"></Icon>
+              <Icon type="drag"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/link','友情链接')">友情链接管理</span>
             </Menu-item>
             <Menu-item name="域名管理">
-              <Icon type="android-globe"></Icon>
+              <Icon type="android-globe"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/domain','域名管理')">域名管理</span>
             </Menu-item>
             <Menu-item name="公共代码管理">
-              <Icon type="social-javascript"></Icon>
+              <Icon type="social-javascript"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/code','公共代码管理')">公共代码管理</span>
             </Menu-item>
             <Menu-item name="联系方式管理">
-              <Icon type="ios-telephone"></Icon>
+              <Icon type="ios-telephone"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/contactway','联系方式管理')">联系方式管理</span>
             </Menu-item>
             <Menu-item name="主题/活动创意">
-              <Icon type="beer"></Icon>
+              <Icon type="beer"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/activity','主题/活动创意')">主题/活动创意管理</span>
             </Menu-item>
             <Menu-item name="模板管理">
-              <Icon type="map"></Icon>
+              <Icon type="map"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/template','模板管理')">模板管理</span>
             </Menu-item>
           </Submenu>
           <Submenu name="6">
             <template slot="title">
-              <Icon type="pinpoint"></Icon>
+              <Icon type="pinpoint"  ></Icon>
               站点/用户管理
             </template>
             <Menu-item name="站点分类">
-              <Icon type="android-cloud-circle"></Icon>
+              <Icon type="android-cloud-circle"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/sitetype','站点分类')">站点分类</span>
             </Menu-item>
             <Menu-item name="站点管理">
-              <Icon type="android-cloud-circle"></Icon>
+              <Icon type="android-cloud-circle" ></Icon>
               <span class="layout-text" @click="routerChange('/admin/site','站点管理')">站点管理</span>
             </Menu-item>
             <Menu-item name="模板管理">
-              <Icon type="map"></Icon>
+              <Icon type="map"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/templatemanage','站点模板管理')">站点模板管理</span>
             </Menu-item>
 
             <Menu-item name="SEO属性">
-              <Icon type="map"></Icon>
+              <Icon type="map"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/seo','SEO属性')">搜索引擎优化管理</span>
             </Menu-item>
             <Menu-item name="站点静态化配置">
-              <Icon type="android-cloud-circle"></Icon>
+              <Icon type="android-cloud-circle"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/staticconfig','站点静态化配置')">站点静态化配置</span>
             </Menu-item>
             <Menu-item name="用户管理">
-              <Icon type="person"></Icon>
+              <Icon type="person"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/siteuser','用户管理')">用户管理</span>
             </Menu-item>
           </Submenu>
           <Submenu name="7">
             <template slot="title">
-              <Icon type="ios-analytics"></Icon>
+              <Icon type="ios-analytics"  ></Icon>
               数据统计
             </template>
             <Menu-item name="浏览量统计">
-              <Icon type="android-cloud-circle"></Icon>
+              <Icon type="android-cloud-circle"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/pv','浏览量统计')">浏览量统计</span>
             </Menu-item>
             <Menu-item name="浏览量展示">
-              <Icon type="android-cloud-circle"></Icon>
+              <Icon type="android-cloud-circle"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/show','浏览量展示')">浏览量展示</span>
             </Menu-item>
             <Menu-item name="爬虫统计">
@@ -230,21 +233,21 @@
               <span class="layout-text" @click="routerChange('/admin/crawler','爬虫统计')">爬虫统计</span>
             </Menu-item>
             <Menu-item name="甩单展示">
-              <Icon type="android-cloud-circle"></Icon>
+              <Icon type="android-cloud-circle"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/rejection','甩单展示')">甩单展示</span>
             </Menu-item>
             <Menu-item name="搜索引擎关键词统计">
-              <Icon type="android-cloud-circle"></Icon>
+              <Icon type="android-cloud-circle"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/countkeyword','搜索关键词统计')">搜索关键词统计</span>
             </Menu-item>
             <Menu-item name="其他统计">
-              <Icon type="android-cloud-circle"></Icon>
+              <Icon type="android-cloud-circle"  ></Icon>
               <span class="layout-text" @click="routerChange('/admin/other','其他统计')">其他统计</span>
             </Menu-item>
           </Submenu>
         </Menu>
       </i-col>
-      <i-col span="20">
+      <i-col :span="spanRight" style="overflow: auto">
         <div class="layout-header">
           <Row type="flex" justify="end" align="middle" class="code-row-bg">
             <Col span="2" align="right">
@@ -296,6 +299,8 @@
   export default {
     data() {
       return {
+        spanLeft: 4,
+        spanRight: 20,
         activeName: '',
         sysname: '',
         count: '无',
@@ -306,6 +311,7 @@
       changepwd,
       logout
     },
+
     methods: {
       menuClick(e) {
 
@@ -332,6 +338,7 @@
       logOut() {
         this.$refs.logout.modal = true
       },
+
       messageLog() {
         router.replace('/admin/messageLog');
       }
