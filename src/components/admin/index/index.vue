3d8e14c4d60f57194458acf62c82a404a9edf20e
yangjingzheng <1318911846@qq.com>
2017-09-30 14:58:58 +0800

    home edit

--- src/components/admin/index/index.vue
+++ src/components/admin/index/index.vue
@@ -6,20 +6,20 @@
         <div class="homehead">
           <div style="padding-bottom: 10px">
             <Icon style="font-size: 30px;" type="podium"></Icon>
-            <span class="footfont" style="display: inline-block;margin-left:15px;font-size: 25px;color: #FF0000;">{{pv}}</span>
+            <span class="footfont" style="color: #FF0000;">{{pv}}</span>
           </div>
           <div style="font-size: 15px;">今日浏览量（pv)</div>
-          <div style="margin-top: 10px;width:120px;border:3px solid rgba(255, 223, 37, 1);"></div>
+          <div class="footborder" style="border:3px solid rgba(255, 223, 37, 1);"></div>
         </div>
         </Col>
         <Col span="4">
         <div style="height: 130px;padding: 15px">
           <div style="padding-bottom: 10px">
             <Icon style="font-size: 30px;" type="bug"></Icon>
-            <span style="display: inline-block;margin-left:15px;font-size: 25px;color: #66FFFF;">{{useragent}}</span>
+            <span class="footfont" style="color: #66FFFF;">{{useragent}}</span>
           </div>
           <div style="font-size: 15px;">今日爬取（spider）</div>
-          <div style="margin-top: 10px;width:120px;border:3px solid  rgba(51, 204, 255, 1);"></div>
+          <div class="footborder" style="border:3px solid  rgba(51, 204, 255, 1);"></div>
         </div>
 
         </Col>
@@ -27,10 +27,10 @@
         <div style="height: 130px;padding: 15px">
           <div style="padding-bottom: 10px">
             <Icon style="font-size: 30px;" type="android-person-add"></Icon>
-            <span style="display: inline-block;margin-left:15px;font-size: 25px;color: #003399;">{{article}}</span>
+            <span class="footfont" style="color: #003399;">{{article}}</span>
           </div>
           <div style="font-size: 15px;">今日添加文章数量</div>
-          <div style="margin-top: 10px;width:120px;border:3px solid   rgba(51, 0, 204, 1);"></div>
+          <div class="footborder" style="border:3px solid   rgba(51, 0, 204, 1);"></div>
         </div>
         </Col>
 
@@ -38,10 +38,10 @@
         <div style="height: 130px;padding: 15px">
           <div style="padding-bottom: 10px">
             <Icon style="font-size: 30px;" type="ios-search-strong"></Icon>
-            <span style="display: inline-block;margin-left:15px;font-size: 25px;color: #FF6600;">{{shuaidan}}</span>
+            <span class="footfont" style="color: #FF6600;">{{shuaidan}}</span>
           </div>
           <div style="font-size: 15px;">今日询盘数量</div>
-          <div style="margin-top: 10px;width:120px;border:3px solid  rgba(255, 102, 102, 1);"></div>
+          <div class="footborder" style="border:3px solid  rgba(255, 102, 102, 1);"></div>
         </div>
         </Col>
 
@@ -49,35 +49,35 @@
         <div style="height: 130px;padding: 15px">
           <div style="padding-bottom: 10px">
             <Icon style="font-size: 30px;" type="ios-book"></Icon>
-            <span style="display: inline-block;margin-left:15px;font-size: 25px;color: #199ED8;">{{shoulu}}</span>
+            <span class="footfont" style="color: #199ED8;">{{shoulu}}</span>
           </div>
           <div style="font-size: 15px;">文章收录总量</div>
-          <div style="margin-top: 10px;width:120px;border:3px solid   rgba(0, 0, 255, 1);"></div>
+          <div class="footborder" style="border:3px solid   rgba(0, 0, 255, 1);"></div>
         </div>
         </Col>
       </Row>
     </div>
-    <div style="width:100%;height: 315px;box-sizing: border-box">
+    <div class="centercss">
       <pv ref="pv" style="height: 315px;background-color:white;display: inline-block"></pv>
       <engine ref="engine" style="height: 315px;background-color:white;display: inline-block"></engine>
       <keyword ref="keyword" style="height: 315px;background-color:white;display: inline-block"></keyword>
     </div>
     <div>
       <Row :gutter="60" style="margin: 10px">
-        <Col span="11" style="background-color: white;margin:10px;height: 250px">
+        <Col span="11" class="eventcss">
         <h2>
           <Icon type="ios-calendar-outline"></Icon>&nbsp;&nbsp;营销模式
         </h2>
         <Carousel autoplay v-model="value2">
           <CarouselItem v-for="(item,index) in marketdata" :key="index" @click="show(index)">
-            <div @click="show(index)" style="height: 190px;cursor:pointer">
-              <img style="display:block;margin: 0 auto" :src=formatter_str(item.img) :alt="item.title" >
+            <div @click="show(index)" class="eventmouse">
+              <img style="display:block;margin: 0 auto" :src=formatter_str(item.img) :alt="item.title">
             </div>
-          <div  style="text-align: center;font-size: 15px;margin-bottom: 10px">{{item.title}}</div>
+            <div class="eventfont" >{{item.title}}</div>
           </CarouselItem>
         </Carousel>
         </Col>
-        <Col span="12" style="background-color: white;margin:10px;height: 250px">
+        <Col span="12" class="companycss">
         <div style="padding: 30px">
           <div style="padding: 6px"><h1>乐销易技术服务中心</h1></div>
           <div style="padding: 6px"><span style="font-size: 15px"><Icon type="paper-airplane"></Icon>&nbsp;&nbsp;北京易至信科技有限公司</span>
@@ -209,9 +209,38 @@
     padding: 15px
 
   }
-  .footfont{
 
+  .footfont {
+    display: inline-block;
+    margin-left: 15px;
+    font-size: 25px;
   }
 
+  .footborder {
+    margin-top: 10px;
+    width: 120px;
+  }
 
+  .centercss {
+    width: 100%;
+    height: 315px;
+    box-sizing: border-box
+  }
+
+  .eventcss {
+    background-color: white;
+    margin: 10px;
+    height: 250px
+  }
+
+  .eventmouse {
+    height: 190px;
+    cursor: pointer
+  }
+  .eventfont{
+    text-align: center;font-size: 15px;margin-bottom: 10px
+  }
+  .companycss{
+    background-color: white;margin:10px;height: 250px
+  }
 </style>
