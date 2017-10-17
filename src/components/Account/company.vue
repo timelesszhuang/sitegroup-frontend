e3ff2f3d97013b3ebda5ac058e18fe1b89c6b31a
yangjingzheng <1318911846@qq.com>
2017-10-17 16:55:47 +0800

    company

--- src/components/Account/company.vue
+++ src/components/Account/company.vue
@@ -192,12 +192,21 @@
     },
     methods: {
       Path() {
+        if(!this.form.business_license){
+          return ROOTHOST;
+        }
         return ROOTHOST + this.form.business_license;
       },
       personPath() {
+        if(!this.form.artificialperson_id){
+          return ROOTHOST;
+        }
         return ROOTHOST + this.form.artificialperson_id;
       },
       trademarkPath() {
+        if(!this.form.trademark_img){
+          return ROOTHOST;
+        }
         return ROOTHOST + this.form.trademark_img;
       },
       getResponse(response, file, filelist) {
