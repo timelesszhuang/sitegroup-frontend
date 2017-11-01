3910f1ee869dd8ef33b9ae9c7d4e229520713df9
yangjingzheng <1318911846@qq.com>
2017-11-01 17:14:25 +0800

    showhtml

--- src/components/admin/product/showhtml.vue
+++ src/components/admin/product/showhtml.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <Modal
-      v-model="modal" width="600">
+      v-model="modal" width="300">
       <p slot="header">
         <span>预览</span>
       </p>
-      <div style="width:500px;text-align:center;margin: 0 auto" v-for="(item,index) in this.form">
-     <div style="font-size: 15px"> <a :href=item.url :title=item.site_name target="_blank">{{item.site_name}}预览</a></div>
+      <div style="text-align:left;margin:0 auto;line-height:30px" v-for="(item,index) in this.form">
+     <div style="font-size: 15px"> <a :href=item.url :title=item.site_name target="_blank">{{item.site_name}}预览 <Icon type="forward"></Icon></a></div>
       </div>
       <div slot="footer">
         <Button type="success" size="large" :loading="modal_loading" @click="close">关闭</Button>
