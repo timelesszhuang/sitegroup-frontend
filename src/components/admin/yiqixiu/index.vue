17b3ffb4d662b19983db612bc08bd6dfeb843549
2301887641 <2301887641@qq.com>
2017-11-16 18:13:48 +0800

    公众

--- src/components/admin/yiqixiu/index.vue
+++ src/components/admin/yiqixiu/index.vue
@@ -1,9 +1,35 @@
 <template>
     <div>
-      <iframe src="http://www.yqx.com" frameborder="0" width="100%" height="100%"></iframe>
+      <iframe :src="iframe" frameborder="0" width="100%" height="100%"></iframe>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import http from '../../../assets/js/http.js';
 
+  export default{
+      data(){
+        return {
+          iframe:''
+        }
+      },
+    created() {
+        this.index()
+    },
+      methods:{
+          index() {
+            this.apiGet('yiqixiu/').then((res) => {
+              this.handelResponse(res, (data, msg) => {
+                this.iframe=data
+              }, (data, msg) => {
+                this.$Message.error(msg);
+              })
+            }, (res) => {
+              //处理错误信息
+              this.$Message.error('网络异常，请稍后重试。');
+            })
+          }
+       },
+    mixins: [http]
+    }
 </script>
