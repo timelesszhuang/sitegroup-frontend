831eab1ff61bdb8a875eae8dae94365b84769429
yangjingzheng <1318911846@qq.com>
2017-11-17 10:01:30 +0800

    yiqixiu

--- src/components/admin/yiqixiu/index.vue
+++ src/components/admin/yiqixiu/index.vue
@@ -1,35 +1,46 @@
 <template>
-    <div style="height:100%">
-      <iframe :src="iframe" frameborder="0" width="100%" height="100%"></iframe>
-    </div>
+
+  <div class="accept-container" style="height:auto">
+    <iframe :src="iframe" v-show="iframeState" id="show-iframe" width="100%" frameborder=0     name="showHere"
+            scrolling=auto></iframe>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
   import http from '../../../assets/js/http.js';
 
-  export default{
-      data(){
-        return {
-          iframe:''
-        }
-      },
-    created() {
-        this.index()
+  export default {
+    data() {
+      return {
+        iframeState: true,
+        iframe: ''
+      }
     },
-      methods:{
-          index() {
-            this.apiGet('yiqixiu/').then((res) => {
-              this.handelResponse(res, (data, msg) => {
-                this.iframe=data
-              }, (data, msg) => {
-                this.$Message.error(msg);
-              })
-            }, (res) => {
-              //处理错误信息
-              this.$Message.error('网络异常，请稍后重试。');
-            })
-          }
-       },
+    created() {
+      this.index()
+    },
+    mounted() {
+      const oIframe = document.getElementById('show-iframe');
+      //const deviceWidth = document.documentElement.clientWidth;
+      const deviceHeight = document.documentElement.clientHeight;
+      //oIframe.style.width = deviceWidth + 'px';
+      oIframe.style.height = deviceHeight + 'px';
+    },
+
+    methods: {
+      index() {
+        this.apiGet('yiqixiu/').then((res) => {
+          this.handelResponse(res, (data, msg) => {
+            this.iframe = data
+          }, (data, msg) => {
+            this.$Message.error(msg);
+          })
+        }, (res) => {
+          //处理错误信息
+          this.$Message.error('网络异常，请稍后重试。');
+        })
+      }
+    },
     mixins: [http]
-    }
+  }
 </script>
