dd648127ee3d79729e8e405b04a3b9c20f859205
yangjingzheng <1318911846@qq.com>
2017-12-15 18:09:35 +0800

    company

--- src/components/admin/imglist/index.vue
+++ src/components/admin/imglist/index.vue
@@ -2,10 +2,7 @@
   <div>
     <div class="top">
       分类名:
-      <Input v-model="name" placeholder="产品名" style="width:300px;"></Input>
-      <Select v-model="type_id" style="width:200px;" placeholder="根据分类查询" label-in-value filterable clearable>
-        <Option v-for="item in ptype" :value="item.id" :key="item">{{ item.text }}</Option>
-      </Select>
+      <Input v-model="name" placeholder="图集名" style="width:300px;"></Input>
       <Button type="primary" @click="queryData">查询</Button>
       <Button type="success" @click="add">添加</Button>
     </div>
@@ -24,10 +21,9 @@
         </div>
       </div>
     </div>
-    <padd ref="add" :ptype="ptype"></padd>
-    <psave ref="save" :ptype="ptype" :form="editinfo"></psave>
+    <padd ref="add" ></padd>
+    <psave ref="save"  :form="editinfo"></psave>
     <editimg ref="editimg" :form="imginfo"></editimg>
-    <showhtml ref="showhtml" :form="showhtmldata"></showhtml>
   </div>
 </template>
 
@@ -36,13 +32,11 @@
   import padd from './add.vue';
   import psave from './save.vue';
   import editimg from './editimg.vue';
-  import showhtml from './showhtml.vue'
 
 
   export default {
     data() {
       return {
-        action: HOST + 'admin/uploadProductSerImg',
         self: this,
         border: true,
         stripe: true,
@@ -57,15 +51,11 @@
         datas: [],
         editinfo: {},
         imginfo: {},
-        ptype: [],
-        type_id: '',
-        showhtmldata:[]
       }
     },
-    components: {padd, psave, editimg,showhtml},
+    components: {padd, psave, editimg},
     created() {
       this.getData();
-      this.getproducttype()
     },
     methods: {
       getData() {
@@ -77,7 +67,7 @@
             type_id: this.type_id
           }
         }
-        this.apiGet('admin/product', data).then((data) => {
+        this.apiGet('imglist', data).then((data) => {
           this.handelResponse(data, (data, msg) => {
             this.datas = data.rows
             this.total = data.total;
@@ -99,45 +89,66 @@
       queryData() {
         this.getData();
       },
-      getproducttype(func) {
-        this.apiGet('admin/getProductType').then((res) => {
-          this.handelResponse(res, (data, msg) => {
-            this.ptype = data;
-          }, (data, msg) => {
-            this.$Message.error(msg);
-          })
-        }, (res) => {
-          //处理错误信息
-          this.$Message.error('网络异常，请稍后重试。');
-        });
-      },
       add() {
         this.$refs.add.modal = true
       },
-      showhtml(index) {
-        let data = this.datas[index]
-        this.apiPost('productshowhtml', data).then((res) => {
-          this.handelResponse(res, (data, msg) => {
-            if (data.length == 1) {
-              window.open(data[0].url);
-            } else {
-              this.showhtmldata = data;
-              this.$refs.showhtml.modal = true
-            }
-            this.modal = false;
-          }, (data, msg) => {
-            this.modal_loading = false;
-            this.$Message.error(msg);
-          })
-        }, (res) => {
-          //处理错误信息
-          this.modal_loading = false;
-          this.$Message.error('网络异常，请稍后重试。');
+      start(params) {
+        let editid = params.row.id
+        let _this = this
+        this.$Modal.confirm({
+          title: '确认',
+          content: '您确定启用',
+          okText: '确认',
+          cancelText: '取消',
+          onOk: () => {
+            _this.apiGet('changeimgliststatus/' + editid + "/10").then((res) => {
+              _this.handelResponse(res, (data, msg) => {
+                _this.getData()
+                _this.$Message.success(msg);
+              }, (data, msg) => {
+                _this.modal_loading = false;
+                _this.$Message.error(msg, 5);
+              })
+            }, (res) => {
+              //处理错误信息
+              _this.$Message.error('网络异常，请稍后重试');
+            })
+          },
+          onCancel: () => {
+            return false
+          }
+        })
+      },
+      stop(params) {
+        let editid = params.row.id
+        let _this = this
+        this.$Modal.confirm({
+          title: '确认',
+          content: '您确定禁用',
+          okText: '确认',
+          cancelText: '取消',
+          onOk: () => {
+            _this.apiGet('changeimgliststatus/' + editid + "/20").then((res) => {
+              _this.handelResponse(res, (data, msg) => {
+                _this.getData()
+                _this.$Message.success(msg);
+              }, (data, msg) => {
+                _this.modal_loading = false;
+                _this.$Message.error(msg, 5);
+              })
+            }, (res) => {
+              //处理错误信息
+              _this.$Message.error('网络异常，请稍后重试');
+            })
+          },
+          onCancel: () => {
+            return false
+          }
         })
       },
       edit(index) {
         let editid = this.datas[index].id
-        this.apiGet('admin/product/' + editid).then((res) => {
+        this.apiGet('imglist/' + editid).then((res) => {
           this.handelResponse(res, (data, msg) => {
             this.editinfo = data
             this.modal = false;
@@ -152,7 +163,7 @@
       },
       editimg(index) {
         let editid = this.datas[index].id
-        this.apiGet('admin/getProductImgList/' + editid).then((res) => {
+        this.apiGet('getimgser/' + editid).then((res) => {
           this.handelResponse(res, (data, msg) => {
             this.imginfo = data
             if (data.imglist && data.imglist.length) {
@@ -174,6 +185,7 @@
     computed: {
       tableColumns() {
         let columns = [];
+        let _this = this
         if (this.showCheckbox) {
           columns.push({
             type: 'selection',
@@ -188,49 +200,103 @@
             align: 'center'
           })
         }
-        columns.push({
-          title: '缩略图',
-          width:'200',
-          key: 'base64',
-          sortable: true,
-          render(row, index) {
-            var type = ' <img  style="max-width: 190px;max-height: 150px"  src="'+row.image+'" alt="">';
-            return type;
-          },
-        });
+
         columns.push({
           width:'150',
-          title: '编号',
-          key: 'sn',
+          title: '图集名称',
+          key: 'name',
         });
 
         columns.push({
-          title: '产品名称',
-          key: 'name',
-          sortable: true
-        });
-        columns.push({
-          title: '所属分类',
-          key: 'type_name',
-          sortable: true
-        });
-        columns.push({
-          title: '收费方式',
-          key: 'payway',
+          title: '英文名称',
+          key: 'en_name',
           sortable: true
         });
         columns.push(
           {
             title: '操作',
             key: 'action',
-            width: 250,
+            width: 350,
             align: 'center',
             fixed: 'right',
-            render(row, column, index) {
-              return `<i-button type="primary" size="small" @click="edit(${index})">修改</i-button>
-                      <i-button type="info" size="small" @click="editimg(${index})">修改产品图片</i-button>
-&nbsp;<i-button type="error" size="small" @click="showhtml(${index})">页面预览</i-button>&nbsp;`;
-            }
+            render(h, params) {
+              let statusbutton = '';
+              if (params.row.status == '20') {
+                //20 状态为禁用 应该启用
+                statusbutton = h('Button', {
+                  props: {
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  attrs: {
+                    type: 'info'
+                  },
+                  on: {
+                    click: function () {
+                      _this.start(params)
+                    }
+                  }
+                }, '启用')
+              } else {
+                statusbutton = h('Button', {
+                  props: {
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  attrs: {
+                    type: 'error'
+                  },
+                  on: {
+                    click: function () {
+                      _this.stop(params)
+                    }
+                  }
+                }, '禁用')
+              }
+              return h('div', [
+                h('Button', {
+                  props: {
+                    size: 'small'
+                  },
+                  attrs: {
+                    type: 'primary'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: function () {
+                      if (params.row.url) {
+                        _this.urledit(params.index)
+                      } else {
+                        _this.edit(params.index)
+                      }
+                    }
+                  }
+                }, '修改'),
+                h('Button', {
+                  props: {
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  attrs: {
+                    type: 'info'
+                  },
+                  on: {
+                    click: function () {
+                      _this.editimg(params.index)
+                    }
+                  }
+                }, '修改图集'),
+                statusbutton
+              ]);
+            },
           }
         );
         return columns;
