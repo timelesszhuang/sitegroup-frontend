a02991c15b3119463229d315a5086ce8b1bb61fe
yangjingzheng <1318911846@qq.com>
2017-12-18 09:56:14 +0800

    company

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ Vue.use(ElementUI)
 
 // let domain = 'local.sitegroup.com'
 
-let domain = 'local.sitegroup.com'
+let domain = 'api.salesman.cc'
 
 var HOST = 'http://' + domain + '/index.php/'
 var ROOTHOST = 'http://' + domain + '/'
