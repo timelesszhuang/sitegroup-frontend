06a4630158a340becc45fad56f2279279cc3ff12
yangjingzheng <1318911846@qq.com>
2017-12-18 15:20:43 +0800

    company

--- src/components/admin/nav/tab.vue
+++ src/components/admin/nav/tab.vue
@@ -3,9 +3,6 @@
     <Tab-pane label="栏目管理" name="menu">
       <naviga ref="menu"></naviga>
     </Tab-pane>
-    <Tab-pane label="栏目管理" name="navigation">
-      <navigation ref="men"></navigation>
-    </Tab-pane>
     <Tab-pane label="栏目分类管理" name="Menutype">
       <Menutype ref="menutype"></Menutype>
     </Tab-pane>
@@ -13,18 +10,14 @@
 </template>
 <script>
   import http from '../../../assets/js/http.js'
-  import navigation from '../navigation/index.vue'
   import Menutype from '../menutype/index.vue'
   import naviga from '../menu/index.vue'
 
   export default {
-    components: {navigation, Menutype,naviga},
+    components: { Menutype,naviga},
     methods: {
       getdata(name) {
-        if (name == "navigation") {
-          this.$refs.men.getData()
-          this.$refs.men.getmenutype()
-        } else if (name == "Menutype") {
+       if (name == "Menutype") {
           this.$refs.menutype.getData()
         } else if (name == 'menu') {
           this.$refs.menu.getmenutype()
