8d23efa753343b89a1749bdc6789e122e8643b16
timelessz <834916321@qq.com>
2018-01-25 09:08:25 +0800

    Merge branch 'dev' of https://github.com/timelesszhuang/sitegroup-frontend into dev
    # Conflicts:
    #	src/main.js
    
    首页相关页面 重新设计

--- src/main.js
+++ src/main.js
@@ -11,8 +11,6 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import Drilldown from '../node_modules/highcharts/modules/drilldown.js'
 import Highcharts from 'highcharts'
-import Waterfall from 'vue-waterfall/lib/waterfall'
-import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
 Drilldown(Highcharts);
 import VueQriously from 'vue-qriously'
 
